5b288f0c5075be9fd1bb3eda873b0743d6349dca
herfel97 <herfeli97@gmail.com>
2026-05-17 12:43:29 -0500

    Changes in page

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Contador es: {count}
         </button>
       </section>
 
