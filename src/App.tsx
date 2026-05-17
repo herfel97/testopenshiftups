d8ce24a67f5330322b6909673072163013f9411c
Hernan Morocho <herfeli97@gmail.com>
2026-05-17 13:48:10 -0500

    chore(ui): actualizar etiqueta del botón para prueba gihp4c

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          prueba de sync contenedor {count}
+          prueba gihp4c {count}
         </button>
       </section>
 
