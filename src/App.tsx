b3676953ab577fb505fcf22d03ace2cc0d053430
herfel97 <herfeli97@gmail.com>
2026-05-17 13:01:06 -0500

    prueba de despliegue sin autosync

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          prueba de sync {count}
+          prueba de sync contenedor {count}
         </button>
       </section>
 
