2619c9d837acd7b551529d3929c6ce90f7ad9956
herfel97 <herfeli97@gmail.com>
2026-05-17 12:55:41 -0500

    prueba de

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          {count}
+          prueba de sync {count}
         </button>
       </section>
 
