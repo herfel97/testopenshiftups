08d7b4719094087b04addd28ccee6748e2ca66a9
Hernan Morocho <herfeli97@gmail.com>
2026-05-19 16:15:11 -0500

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          prueba dev {count}
+          prueba dev GIHH {count}
         </button>
       </section>
 
