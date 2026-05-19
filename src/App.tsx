f19ff10e0a8c80202d148c2f77d19da8d80b41c7
Hernan Morocho <herfeli97@gmail.com>
2026-05-19 14:47:12 -0500

    tests adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          prueba gihp4c {count}
+          prueba deploy {count}
         </button>
       </section>
 
