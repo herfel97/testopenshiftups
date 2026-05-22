d896a2ec625b914806504beb4b0f8ab014b79139
Hernan Morocho <herfeli97@gmail.com>
2026-05-22 11:08:23 -0500

    add testd

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          prueba developer {count}
+          prueba developer gihp4c {count}
         </button>
       </section>
 
