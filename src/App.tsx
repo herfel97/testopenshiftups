6380e98c4d58a3b7cef140d8d342834ea123003b
Hernan Morocho <herfeli97@gmail.com>
2026-05-22 11:17:49 -0500

    changes msg text

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          prueba developer gihp4c {count}
+          prueba developer UPS gihp4c {count}
         </button>
       </section>
     </>
