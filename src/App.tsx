d056aa2a67eff47523e099061be2fb993740db6c
Hernan Morocho <herfeli97@gmail.com>
2026-07-24 12:50:49 -0500

    changes msg title

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          test despliegue UPS tesis {count}
+          test despliegue UPS tesis 2026 prueba final{count}
         </button>
       </section>
     </>
